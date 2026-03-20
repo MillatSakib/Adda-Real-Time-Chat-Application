a3f02d7a56a524a7d2fb3febc75be11d042af174
Sohan <millatsakib01@gmail.com>
2026-03-20 23:33:12 +0600

    Navbar add

---
+++ Frontend/src/store/useAuthStore.js
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdsatingProfile: false,
+  isCheckingAuth: true,
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data.user, isCheckingAuth: false });
+    } catch (err) {
+      set({ authUser: null, isCheckingAuth: false });
+    }
+  },
+}));
